fb0d8c9f0e5a9a6095ed422d604afb05815ee1b6
Miguel <mj601586@gmail.com>
2026-04-23 07:50:46 -0600

    Commit strategy completo

--- src/main/java/com/ux/strategy/context/Navigator.java
+++ src/main/java/com/ux/strategy/context/Navigator.java
@@ -19,7 +19,27 @@ public class Navigator {
         strategy.buildRoute("CDMX", "New York");
 
         scanner sc = new Scanner(System.in);
-        System.out.println
+        System.out.println("seleccione el tipo de navegacion: 1. Road, 2. PublicTransport, 3. Walking, 4. Flying");
+        int option = sc.nextInt();
+
+        switch (option) {
+            case 1:
+                strategy = new RoadStrategy();
+                break;
+                case 2:
+                    strategy = new PublicTransportStrategy();
+                    break;
+                    case 3:
+                        strategy = new WalkingStrategy();
+                        break;
+                        case 4:
+                            strategy = new FlyStrategy();
+                            break;
+
+                            default:
+                                System.out.println("Opcion no savlida. Usando RoadStrategy por defecto");
+                                strategy = new RoadStrategy();
+        }
     }
 
 
